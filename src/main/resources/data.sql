ebabeeb326c60c71ffc6889425c88cdcb6b8fee6
ricardo-tonietti <rictonietti@gmail.com>
2023-02-24 14:58:27 -0300

    update token

---
+++ src/main/resources/data.sql
@@ -0,0 +1,69 @@
+-- Database: ead-authuser-v2
+
+-- DROP DATABASE IF EXISTS "ead-authuser-v2";
+
+CREATE DATABASE "ead-authuser-v2"
+    WITH
+    OWNER = postgres
+    ENCODING = 'LATIN1'
+    LC_COLLATE = 'en_US'
+    LC_CTYPE = 'en_US'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+-- Database: ead-address-v2
+
+-- DROP DATABASE IF EXISTS "ead-address-v2";
+
+CREATE DATABASE "ead-address-v2"
+    WITH
+    OWNER = postgres
+    ENCODING = 'LATIN1'
+    LC_COLLATE = 'en_US'
+    LC_CTYPE = 'en_US'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+-- Database: ead-course-v2
+
+-- DROP DATABASE IF EXISTS "ead-course-v2";
+
+CREATE DATABASE "ead-course-v2"
+    WITH
+    OWNER = postgres
+    ENCODING = 'LATIN1'
+    LC_COLLATE = 'en_US'
+    LC_CTYPE = 'en_US'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+-- Database: ead-notification-v2
+
+-- DROP DATABASE IF EXISTS "ead-notification-v2";
+
+CREATE DATABASE "ead-notification-v2"
+    WITH
+    OWNER = postgres
+    ENCODING = 'LATIN1'
+    LC_COLLATE = 'en_US'
+    LC_CTYPE = 'en_US'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+-- Database: ead-payment-v2
+
+-- DROP DATABASE IF EXISTS "ead-payment-v2";
+
+CREATE DATABASE "ead-payment-v2"
+    WITH
+    OWNER = postgres
+    ENCODING = 'LATIN1'
+    LC_COLLATE = 'en_US'
+    LC_CTYPE = 'en_US'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
